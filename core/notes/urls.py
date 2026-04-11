967d085370b93ccc0f381a27dcecb1ec6fad4a56
Abhay Singh <abhaysingh@Abhays-MacBook-Air.local>
2026-04-11 16:07:53 +0530

    viewset and router added

--- core/notes/urls.py
+++ core/notes/urls.py
@@ -1,11 +1,28 @@
-from django.urls import path
+from django.urls import path ,include 
+from rest_framework.routers import DefaultRouter # for the viewset class 
+from . import views
 
-from . import  views
 
-urlpatterns = [
+# urlpatterns = [
     
-    path('notes/', views.NoteList.as_view(), name= 'get-notes'),
-    path('notes/<int:pk>/', views.NoteDetail.as_view(), name = 'note-detail')  # name = "note-detail" is a nickname used in place of the url 
+#     path('notes/', views.NoteList.as_view(), name= 'get-notes'),
+#     path('notes/<int:pk>/', views.NoteDetail.as_view(), name = 'note-detail')  # name = "note-detail" is a nickname used in place of the url 
    
     
+# ]
+
+
+# Step 1 creating object of DefaultRouter Class
+router = DefaultRouter()
+
+# Step 2 Register the ViewSet
+router.register('notes',views.NoteViewSet,basename='notes')
+
+# Step 3 Include router url
+urlpatterns =[
+    
+    path('',include(router.urls))
+    
 ]
+
+
--- core/notes/views.py
+++ core/notes/views.py
@@ -6,92 +6,86 @@ from .models import Notes
 from .serializers import NoteSerializer
 from rest_framework import status  # for status code 
 from rest_framework.views import APIView # for class based  views
+from rest_framework import viewsets
 
 
 
-class NoteList(APIView):
+
+
+
+
+            
+            
+            
+class NoteViewSet(viewsets.ViewSet):
     
-    # GET - fetch all notes 
-    def get(self,request):
+    # GET get all notes /api/notes
+    def list(self,request):
         note = Notes.objects.all()
         serializer = NoteSerializer(note,many = True)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.data,status = status.HTTP_200_OK)
     
-    #POST - Create a note. recieve data --> validate --> save in db 
-    def post(self,request):
-        serializer = NoteSerializer(data = request.data)
-        
+    # POST create note /api/notes
+    def create(self,request):
+        serializer = NoteSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response(serializer.data,status= status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
         
-
-# Handles - /api/notes/1 2 3../   
-class NoteDetail(APIView):
-    
-    #helper function for finding note exists or not  used by all methods inside Note Detail
-    def get_object(self,pk):
+    # GET single note /api/notes/1 2 3 4...
+    def retrieve(self,request,pk = None):
         try:
-            return Notes.objects.get(pk = pk)
+            note = Notes.objects.get(pk = pk)
         except Notes.DoesNotExist:
-            return None
+            return  Response({"error ":"Note Not Found"},status=status.HTTP_404_NOT_FOUND)
+        serializer = NoteSerializer(note)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+     # PUT update note  /api/notes/1/
+    def update(self,request,pk=None):
+        try:
+            note = Notes.objects.get(pk = pk)
+        except Notes.DoesNotExist:
+            return Response({"Error":"Note Not Found"},status = status.HTTP_404_NOT_FOUND)
+        serializer = NoteSerializer(note,data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    #PATCH update partial /ai/notes/1
+    def partial_update(self,request,pk=None):
+        try:
+            note = Notes.objects.get(pk = pk)
+        except Notes.DoesNotExist:
+            return Response({"Error":"Note Not Found"},status = status.HTTP_404_NOT_FOUND)
+        serializer = NoteSerializer(note,data = request.data ,partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status = status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    
+    #Delete delete the note /api/notes/1 2 3
+    def destroy(self,request,pk = None):
+        try:
+            note = Notes.objects.get(pk = pk)
+        except Notes.DoesNotExist:
+            return Response({"Error":"Note Not Found"},status= status.HTTP_404_NOT_FOUND)
+        note.delete()
+        return Response({"Message":"Note Deleted Successfully"},status=status.HTTP_204_NO_CONTENT)
+         
+          
         
-    # GET - return single note
     
-    def get(self,request,pk):
-          note = self.get_object(pk) # helper function call
-          if note == None:
-              return Response({"Error ":"Note Not Found"},status=status.HTTP_404_NOT_FOUND)
-          else:
-              serializer = NoteSerializer(note)
-              return Response(serializer.data,status=status.HTTP_200_OK)
     
-    #PUT - update entire note 
     
-    def put(self, request,pk):
-        note = self.get_object(pk)
-        if note ==None:
-            return Response({"Error":"Note Not Found"},status=status.HTTP_404_NOT_FOUND)
-        else:
-            serializer = NoteSerializer(note,data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data,status=status.HTTP_200_OK)
-            else:
-                return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-            
-            
-    # PATCH - Partial Update 
     
-    def patch(self,request,pk):
-        note = self.get_object(pk)
-        if note==None:
-            return Response({"Error": "Note Not Found"},status = status.HTTP_404_NOT_FOUND)
-        else:
-            serializer = NoteSerializer(note,data = request.data,partial = True) 
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data,status = status.HTTP_200_OK)
-            else:
-                return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-            
-   
-   # DELETE - Delete the note           
-   
-    def delete(self,request,pk):
-        note = self.get_object(pk)
-        if note == None:
-            return Response({"Error":"Note not Found"},status=status.HTTP_404_NOT_FOUND)
-        else:
-            note.delete()
-            return Response({"Message":"Note Delete Successfully"},status=status.HTTP_204_NO_CONTENT)     
-            
-            
-            
              
-            
 
             
         
