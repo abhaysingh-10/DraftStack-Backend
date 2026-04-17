d7b768bea6e1344a2bd372a8bd4d6cb63afd382f
Abhay Singh <aiwithabhay7474452@gmail.com>
2026-04-17 22:49:54 +0530

    Added Create Function and Update the Serializer

--- core/notes/serializers.py
+++ core/notes/serializers.py
@@ -8,12 +8,31 @@ class SubTaskSerializer(serializers.ModelSerializer):
         fields = ['id','title','completed']
         
 class NoteSerializer(serializers.ModelSerializer):
-    subtasks = SubTaskSerializer(many= True,read_only=True) # nested Serializer subtask field 
+    subtasks = SubTaskSerializer(many= True,read_only=False) # nested Serializer subtask field 
     
     class Meta:
         model =Notes
         fields = ['id','title','content','created_at','subtasks']
         read_only_fields = ['id','created_at']
+        
+    
+    def create(self,validated_data):
+       
+        # Step1  pull out subtasks data from validated_data
+        subtasks_data = validated_data.pop('subtasks',[])
+        
+        # Step 2 create the Note first
+        note =Notes.objects.create(**validated_data)
+        
+        #  Step 3 — loop through subtasks and create each one
+        for subtask in subtasks_data:
+            SubTask.objects.create(note=note,**subtask)
+            
+        return note
+    
+
+
+        
         
     
     
