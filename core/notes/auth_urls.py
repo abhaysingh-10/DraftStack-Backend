4c9e4a6ee8e1c6e6af612739481df306e07f4cd8
Abhay Singh <aiwithabhay7474452@gmail.com>
2026-04-21 15:48:36 +0530

    Updated auth_urls to JWT Auth

--- core/notes/auth_urls.py
+++ core/notes/auth_urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from . import auth_views
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView #JWT Auth
 
 urlpatterns = [
+    
+    #OLD Token Auth 
     path('register/',auth_views.register ,name='register'),
-    path('login/',auth_views.login,name='login'),
-    path('logout/',auth_views.logout,name='logout'),
+    # path('login/',auth_views.login,name='login'),
+    # path('logout/',auth_views.logout,name='logout'),
+    
+    # NEW JWT AUTH
+    path('login/',TokenObtainPairView.as_view()),
+    path('refresh/',TokenRefreshView.as_view()),
+    path('logout/',auth_views.logout),    
     
 ]
