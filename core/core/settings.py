2349367767dea4565b85aa60ad842df229dcde0e
Abhay Singh <aiwithabhay7474452@gmail.com>
2026-04-21 16:49:54 +0530

    Resolved Errors for JWT Authentication

--- core/core/settings.py
+++ core/core/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 REST_FRAMEWORK = {
     
     'DEFAULT_AUTHENTICATION_CLASSES':[
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES':[
         'rest_framework.permissions.IsAuthenticated',
--- core/notes/auth_views.py
+++ core/notes/auth_views.py
@@ -1,9 +1,11 @@
 from rest_framework.decorators import api_view,permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny , IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError
+
 
 
 # REGISTER CREATE NEW USER/ACCOUNT POST
@@ -28,64 +30,26 @@ def register(request):
     # Create the User  
     user = User.objects.create_user(username=username,password=password)
     
-    # Create token for this User
-    token = Token.objects.create(user=user)
+    return Response({"Message":"Account Created Successfully"},status=status.HTTP_201_CREATED)
     
-    return Response({"Message":"Account Created Successfully",
-                     "token":token.key, #send token back
-                     },status=status.HTTP_201_CREATED)
-    
-    
-# LOGIN- get TOKEN  POST
+
+#LOGOUT - JWT AUTHENTICATION 
 
 @api_view(['POST'])
-@permission_classes([AllowAny]) # anyone can register — no token needed
+@permission_classes([IsAuthenticated])
 
-def login(request):
-    
-    # get username password from request
-    username = request.data.get('username')
-    password = request.data.get('password')
-    
-    # checking if both are provided
-    if not username or not password:
-        return Response({"Error":"Username and password are required"},status=status.HTTP_400_BAD_REQUEST)
-    
-    # checking if user exists
-    try:
-        user = User.objects.get(username=username)
-    except User.DoesNotExist:
-        return Response({"Error":"Invalid Credentials"},status=status.HTTP_400_BAD_REQUEST)
-    
-    # checking if password is correct or not 
-    # using check_password it matches the hashed password  present in the DB
-     
-    if not user.check_password(password):
-        return Response({"Error":"Invalid Credentials"},status=status.HTTP_400_BAD_REQUEST)
-    
-    # get or create token 
-    # get_or_create - if token exists return it, if not create new one
-    token , created = Token.objects.get_or_create(user= user)
-    
-    return Response(
-        {
-            "Message":"Login Successful",
-            "token": token.key,
-            
-        },
-        status=status.HTTP_200_OK
-        )
-    
-    
-# LOGOUT - DELETE THE TOKEN
-
-@api_view(["POST"])
 def logout(request):
+    try:
+        refresh_token = request.data.get('refresh')
+        token = RefreshToken(refresh_token)
+        token.blacklist()
+        return Response({"Message":"Logged out Successfully"},status=status.HTTP_200_OK)
     
-    # Delete token from DB - next time user must login again to get new token
-    request.user.auth_token.delete()
-    return Response({"Message":"Logout Successfully"},status=status.HTTP_200_OK)
-
+    except TokenError:
+        return Response({"Error":"Invalid Token"},status=status.HTTP_400_BAD_REQUEST)
+    
+    
+        
 
 
     
